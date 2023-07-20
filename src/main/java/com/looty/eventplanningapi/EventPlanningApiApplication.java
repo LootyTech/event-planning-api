0d5759c2e25111dcd5430aa32c5d9b90f2a6e017
stever410 <ngotanduc410@gmail.com>
2023-07-20 09:58:02 +0700

    Init project

---
+++ src/main/java/com/looty/eventplanningapi/EventPlanningApiApplication.java
@@ -0,0 +1,13 @@
+package com.looty.eventplanningapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventPlanningApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventPlanningApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/looty/eventplanningapi/EventPlanningApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.looty.eventplanningapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventPlanningApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
